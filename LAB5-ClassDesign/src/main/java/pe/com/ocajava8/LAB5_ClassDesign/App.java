d4909a2782ba0d2a6a022acaf541bb8c8916e4c4
omca <calderonomar48@gmail.com>
2018-01-01 22:55:22 -0500

    LAB5-CLASS DESIGN

---
+++ LAB5-ClassDesign/src/main/java/pe/com/ocajava8/LAB5_ClassDesign/App.java
@@ -0,0 +1,13 @@
+package pe.com.ocajava8.LAB5_ClassDesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

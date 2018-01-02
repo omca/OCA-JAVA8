8c65847a424618695ba569b863543f86e83b5179
omca <calderonomar48@gmail.com>
2018-01-01 21:32:12 -0500

    PRIMERA VERTSION

---
+++ LAB1-JavaBuildingBlocks/src/main/java/com/pe/ocajava8/LAB1_JavaBuildingBlocks/App.java
@@ -0,0 +1,13 @@
+package com.pe.ocajava8.LAB1_JavaBuildingBlocks;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
